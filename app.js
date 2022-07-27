1f4ada1d13df08e42afe9af5a83270ad82cfc519
Kiall Thompson-Lane <kialltlane@hotmail.com>
2022-07-27 10:51:47 +0100

    add views and view engine to app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const pug = require("pug");
 const app = express()
 const port = 3000
+const path = require('node:path');
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
